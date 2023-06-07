37af12649b9b696f38f2245e5a8ff81444c58e15
farishta18 <farishtashinwari01@gmail.com>
2023-06-07 12:57:40 -0400

    Hw7

--- src/test/java/SeleniumHw7/Hw1.java
+++ src/test/java/SeleniumHw7/Hw1.java
@@ -11,23 +11,10 @@ goto http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login
 login
 click on PIM
 
-
 from the table choose any ID
 and print out the row number of that id
 
 please make sure that ur code is dynamic enough to cater if another row gets delted , it still prints the row correctly
-    print the row number of the row that has the id 56247A
-        List<WebElement> allIDs = driver.findElements(By.xpath("//table/tbody/tr/td[2]"));
-
-//        look for 56247A
-        for(int i=0;i<allIDs.size();i++){
-            String id = allIDs.get(i).getText();
-            if(id.equals("56258A")){
-                System.out.println("the id is on row number "+(i+1));
-//                WebElement row = driver.findElement(By.xpath(" //table/tbody/tr[" + (i + 1) + "]"));
-//                System.out.println(row.getText());
-
-            }
 
  */
 
@@ -48,12 +35,12 @@ public class Hw1 extends CommonMethods {
         WebElement pimBtn = driver.findElement(By.xpath("//a[@id='menu_pim_viewPimModule']"));
         pimBtn.click();
 
-        List<WebElement> ides = driver.findElements(By.xpath("//table/tbody/tr[8]/td[2]"));
+        List<WebElement> ides = driver.findElements(By.xpath("//table/tbody/tr[7]/td[2]"));
 
         for (int i = 0; i<ides.size(); i++) {
             String id = ides.get(i).getText();
-            if (id.equals("56440A")){
-                System.out.println("the id is on row number "+(i+1));
+            if (id.equals("56586A")) {
+                System.out.println("the id is on row number " + (i + 1));
 
             }
 
